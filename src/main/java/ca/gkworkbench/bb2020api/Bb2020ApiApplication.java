bfb6d8df628d973d06c2dcab51982cdfce2e7cc4
james-m-tubbs <james.m.tubbs@gmail.com>
2020-11-23 21:37:46 -0500

    spring boot initilizr

---
+++ src/main/java/ca/gkworkbench/bb2020api/Bb2020ApiApplication.java
@@ -0,0 +1,13 @@
+package ca.gkworkbench.bb2020api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bb2020ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bb2020ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/gkworkbench/bb2020api/Bb2020ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gkworkbench.bb2020api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bb2020ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
