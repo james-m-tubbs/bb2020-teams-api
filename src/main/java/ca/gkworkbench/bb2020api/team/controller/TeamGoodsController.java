deef7d9da4c9c8a70888449b1b38409da21548bf
james-m-tubbs <james.m.tubbs@gmail.com>
2020-12-19 20:35:37 -0500

    created endpoints for team goods add/remove

--- src/main/java/ca/gkworkbench/bb2020api/team/controller/TeamGoodsController.java
+++ src/main/java/ca/gkworkbench/bb2020api/team/controller/TeamGoodsController.java
@@ -45,4 +45,95 @@ public class TeamGoodsController {
             );
         }
     }
+
+    @RequestMapping(value = "/api/team/goods/add/cheerleader/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String add_cheerleader_by_team_id(@PathVariable("id") int teamId) {
+        try {
+            TeamVO tVO = tgBO.buyCheerleaderForTeam(teamId);
+            return tBO.getJsonTeam(tVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not Found"
+            );
+        }
+    }
+
+    @RequestMapping(value = "/api/team/goods/remove/cheerleader/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String fire_cheerleader_for_team_id(@PathVariable("id") int teamId) {
+        try {
+            TeamVO tVO = tgBO.fireApothForTeam(teamId);
+            return tBO.getJsonTeam(tVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not Found"
+            );
+        }
+    }
+
+    @RequestMapping(value = "/api/team/goods/add/coach/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String add_coach_by_team_id(@PathVariable("id") int teamId) {
+        try {
+            TeamVO tVO = tgBO.buyCoachForTeam(teamId);
+            return tBO.getJsonTeam(tVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not Found"
+            );
+        }
+    }
+
+    @RequestMapping(value = "/api/team/goods/remove/coach/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String fire_coach_for_team_id(@PathVariable("id") int teamId) {
+        try {
+            TeamVO tVO = tgBO.fireApothForTeam(teamId);
+            return tBO.getJsonTeam(tVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not Found"
+            );
+        }
+    }
+
+    @RequestMapping(value = "/api/team/goods/add/reroll/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String add_reroll_by_team_id(@PathVariable("id") int teamId) {
+        try {
+            TeamVO tVO = tgBO.buyRerollForTeam(teamId);
+            return tBO.getJsonTeam(tVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not Found"
+            );
+        }
+    }
+
+    @RequestMapping(value = "/api/team/goods/remove/reroll/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String remove_reroll_for_team_id(@PathVariable("id") int teamId) {
+        try {
+            TeamVO tVO = tgBO.removeRerollForTeam(teamId);
+            return tBO.getJsonTeam(tVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not Found"
+            );
+        }
+    }
+
+    @RequestMapping(value = "/api/team/goods/add/fans/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String add_fans_for_team_id(@PathVariable("id") int teamId) {
+        try {
+            TeamVO tVO = tgBO.buyDedicatedFans(teamId);
+            return tBO.getJsonTeam(tVO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Not Found"
+            );
+        }
+    }
 }
